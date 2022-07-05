6e93122612839e5a065b42e7f82936af68bfd3a2
dimaredunlap <dimaredunlap@gmail.com>
2022-07-05 13:24:04 +0000

    added jumbotron div

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,11 +8,17 @@ const Home = () => {
 	return (
 		<div className="container-fluid">
 			<div className="navbar">
-				<h1>Landing Page</h1>
+				<h3>Start Bootstrap</h3>
 				<button>Home</button>
 				<button>About</button>
+				<button>Services</button>
 				<button>Contact</button>
 			</div>
+			<div className="Jumbotron">
+				<h1>A Warm Welcome!</h1>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<a href="#" className="btn btn-primary">Call to action!</a>
+			</div>	
 		</div>
 	);
 };
